a8c1b52c5e6668db350c03a5e60dcc3a1a616ee2
Y.Yamamoto <tuki0918@gmail.com>
2025-06-10 08:48:08 +0900

    refactor: simplify image assembly by using blocksToImageBuffer
    (cherry picked from commit ad0db8a62549908c523d48ed6aa92a090b5c38f8)

--- src/utils/imageAssembler.ts
+++ src/utils/imageAssembler.ts
@@ -1,7 +1,7 @@
-import { bufferToPng, placeBlock } from "./block";
+import { blocksToImageBuffer, bufferToPng } from "./block";
 
 /**
- * Common function to generate an image buffer from an array of blocks and convert it to PNG
+ * Generate a PNG buffer from an array of block buffers
  * @param blocks Array of block buffers
  * @param width Image width
  * @param height Image height
@@ -16,30 +16,6 @@ export async function assembleImageFromBlocks(
   blockSize: number,
   channels = 4,
 ): Promise<Buffer> {
-  const imageBuffer = Buffer.alloc(width * height * channels);
-  const blockCountX = Math.ceil(width / blockSize);
-  const blockCountY = Math.ceil(height / blockSize);
-  let blockIndex = 0;
-  for (let by = 0; by < blockCountY; by++) {
-    for (let bx = 0; bx < blockCountX; bx++) {
-      if (blockIndex < blocks.length) {
-        const blockWidth =
-          bx === blockCountX - 1 ? width - bx * blockSize : blockSize;
-        const blockHeight =
-          by === blockCountY - 1 ? height - by * blockSize : blockSize;
-        placeBlock(
-          imageBuffer,
-          blocks[blockIndex],
-          width,
-          bx * blockSize,
-          by * blockSize,
-          blockSize,
-          blockWidth,
-          blockHeight,
-        );
-        blockIndex++;
-      }
-    }
-  }
+  const imageBuffer = blocksToImageBuffer(blocks, width, height, blockSize);
   return await bufferToPng(imageBuffer, width, height, channels);
 }
